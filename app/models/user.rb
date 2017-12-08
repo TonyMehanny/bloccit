083ba67dc89f5f3517c946e9ed4af05781b981bc
Anthony Mehanny <a.mehanny@umiami.edu>
2017-12-08 17:23:47 +0000

    Completed Assignment 23

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
 
    before_save { self.email = email.downcase if email.present? }
+   
+   before_save { 
+     if name
+      name_array = []
+      name.split(" ").each do |x|
+         name_array.push(x.capitalize)
+      end
+      self.name = name_array.join(" ")
+     end
+   }   
+
 
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
    validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
